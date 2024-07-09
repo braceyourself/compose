12fe4d49097c9e84e4d2458c36cca98a11759c8f
braceyourself <ethanabrace@gmail.com>
2024-07-09 17:13:24 -0400

    add continuation

--- src/Concerns/BuildsDockerfile.php
+++ src/Concerns/BuildsDockerfile.php
@@ -44,7 +44,7 @@ trait BuildsDockerfile
         RUN composer dump-autoload \
             && mkdir -p /var/www/html/storage/logs /var/www/html/bootstrap/cache && \
             && chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache && \
-            && chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+            && chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache \
             && composer run-script post-autoload-dump
         
         ENTRYPOINT ["/usr/local/bin/entrypoint.sh"]
