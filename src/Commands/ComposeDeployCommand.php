edf8aca3888690798e818cd66b4444cb8b2125d7
braceyourself <ethanabrace@gmail.com>
2024-07-19 07:28:02 -0400

    try new deployment script

--- src/Commands/ComposeDeployCommand.php
+++ src/Commands/ComposeDeployCommand.php
@@ -123,18 +123,36 @@ class ComposeDeployCommand extends Command
                  ->join(' ');
 
              // restart everything except php
-             Remote::run("docker-compose up -d {$running_services}")->throw();
+             Remote::run("docker-compose up -d {$running_services} --force-recreate --remove-orphans -t0")->throw();
 
             // get the current php container id
             $old_id = $this->getRemoteContainers('php')->first()->ID;
 
             $this->runRemoteScript(<<<BASH
-            docker-compose up -d --no-deps --scale php=2 --no-build --no-recreate php
-            docker-compose exec nginx /usr/sbin/nginx -s reload
-            docker stop {$old_id} 
-            docker-compose up -d --no-deps --scale php=1 --no-build --no-recreate php
-            docker-compose exec nginx /usr/sbin/nginx -s reload
-            docker rm {$old_id}
+            service_name=php
+            old_container_id=$(docker ps -f name=\$service_name -q | tail -n1)
+
+            # bring a new container online, running new code  
+            # (nginx continues routing to the old container only)  
+            docker-compose up -d --no-deps --scale \$service_name=2 --no-recreate \$service_name
+
+            # wait for new container to be available by checking the health
+            new_container_id=$(docker ps -f name=\$service_name -q | tail -n1)
+            while [ "$(docker inspect --format='{{json .State.Health.Status}}' \$new_container_id)" != '"healthy"' ]; do
+                sleep 1
+            done
+
+            # start routing requests to the new container (as well as the old)  
+            docker-compose exec nginx /usr/sbin/nginx -s reload  
+
+            # take the old container offline  
+            docker stop \$old_container_id
+            docker rm \$old_container_id
+
+            docker-compose up -d --no-deps --scale \$service_name=1 --no-recreate \$service_name
+
+            # stop routing requests to the old container  
+            docker-compose exec nginx /usr/sbin/nginx -s reload  
             BASH)->throw();
 
 
