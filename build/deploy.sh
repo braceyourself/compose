8be49cd46f8a212fcf606b06d40f7b1a2c234244
Ethan Brace <ethanabrace@gmail.com>
2024-08-29 13:02:09 -0400

    fix docker_compose calls

--- build/deploy.sh
+++ build/deploy.sh
@@ -13,15 +13,15 @@ else
 fi
 
 service_name=php
-old_container_id=$(docker_compose ps $service_name --format '{{json .ID}}' | tr -d '"' | tail -n1)
+old_container_id=$($docker_compose ps $service_name --format '{{json .ID}}' | tr -d '"' | tail -n1)
 
 # bring a new container online, running new code
 # (nginx continues routing to the old container only)
-docker_compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
+$docker_compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
 
-new_container_id=$(docker_compose ps $service_name --format '{{json .ID}}' | tr -d '"' | tail -n1)
+new_container_id=$($docker_compose ps $service_name --format '{{json .ID}}' | tr -d '"' | tail -n1)
 isHealthy(){
-    echo $(docker_compose ps php --format '{{json .Health}} {{json .ID}}' | grep -qE '"healthy".*'$new_container_id && echo 'yes' || echo 'no')
+    echo $($docker_compose ps php --format '{{json .Health}} {{json .ID}}' | grep -qE '"healthy".*'$new_container_id && echo 'yes' || echo 'no')
 }
 
 # wait for new container to be available by checking the health
@@ -31,16 +31,16 @@ done
 
 
 # start routing requests to the new container (as well as the old)
-docker_compose exec nginx /usr/sbin/nginx -s reload
+$docker_compose exec nginx /usr/sbin/nginx -s reload
 
 # take the old container offline
 docker stop $old_container_id
 docker rm $old_container_id
 
-docker_compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
+$docker_compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
 # todo: remove this reload and set up a public volume
-docker_compose up -d nginx
+$docker_compose up -d nginx
 
 # stop routing requests to the old container
-docker_compose exec nginx /usr/sbin/nginx -s reload
+$docker_compose exec nginx /usr/sbin/nginx -s reload
 
