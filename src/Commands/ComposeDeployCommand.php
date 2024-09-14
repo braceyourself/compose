80bbb960e40f990efd996d1fdc246e85d2bf3919
braceyourself <ethanabrace@gmail.com>
2024-09-14 15:34:14 -0400

    move method to trait

--- src/Commands/ComposeDeployCommand.php
+++ src/Commands/ComposeDeployCommand.php
@@ -338,19 +338,6 @@ class ComposeDeployCommand extends Command
         }
     }
 
-    private function loadServerCredentials(): void
-    {
-        $prompt = function ($text, $default = '', $hint = null) {
-            return text($text,
-                default: $default,
-                hint   : $hint ?? 'Publish and set the compose configuration to avoid this prompt. (artisan vendor:publish --tag=compose-config)'
-            );
-        };
-
-        $this->host = $this->option('host') ?: $this->getOrSetConfig('compose.deploy.host', fn() => $prompt('What is the hostname of the deployment server?'));
-        $this->user = $this->option('user') ?: $this->getOrSetConfig('compose.deploy.user', fn() => $prompt("Enter the user name for '{$this->host}'", exec('whoami')));
-        $this->path = $this->option('path') ?: $this->getOrSetConfig('compose.deploy.path', fn() => $prompt("Enter the path on {$this->host} this app should"));
-    }
 
     private function runArtisanCommand(string $command): void
     {
--- src/Concerns/InteractsWithRemoteServer.php
+++ src/Concerns/InteractsWithRemoteServer.php
@@ -6,11 +6,16 @@ use Illuminate\Support\Stringable;
 use Illuminate\Support\Facades\Cache;
 use Braceyourself\Compose\Facades\Remote;
 use Illuminate\Support\Traits\ForwardsCalls;
+use function Laravel\Prompts\text;
 
 trait InteractsWithRemoteServer
 {
     public function getRemoteEnv($name = null): Stringable
     {
+        if (!isset($this->host)) {
+            $this->loadServerCredentials();
+        }
+
         $env = Cache::store('array')->rememberForever(
             'compose-remote-env' . $this->user . $this->host . $this->path,
             fn() => str(
@@ -27,5 +32,20 @@ trait InteractsWithRemoteServer
         }
 
         return $env;
+
+    }
+
+    private function loadServerCredentials(): void
+    {
+        $prompt = function ($text, $default = '', $hint = null) {
+            return text($text,
+                default: $default,
+                hint   : $hint ?? 'Publish and set the compose configuration to avoid this prompt. (artisan vendor:publish --tag=compose-config)'
+            );
+        };
+
+        $this->host = $this->option('host') ?: $this->getOrSetConfig('compose.deploy.host', fn() => $prompt('What is the hostname of the deployment server?'));
+        $this->user = $this->option('user') ?: $this->getOrSetConfig('compose.deploy.user', fn() => $prompt("Enter the user name for '{$this->host}'", exec('whoami')));
+        $this->path = $this->option('path') ?: $this->getOrSetConfig('compose.deploy.path', fn() => $prompt("Enter the path on {$this->host} this app should"));
     }
 }
