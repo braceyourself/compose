48c931b9c6a576665b3c4aecd3c4c652329fee3a
braceyourself <ethanabrace@gmail.com>
2024-12-07 14:31:54 -0500

    update publish command; ensure compose_profiles env is set

--- src/Commands/ComposePublishCommand.php
+++ src/Commands/ComposePublishCommand.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Artisan;
 use Braceyourself\Compose\Facades\Compose;
+use Braceyourself\Compose\Concerns\InteractsWithEnvFile;
 use Braceyourself\Compose\Concerns\CreatesComposeServices;
 use Braceyourself\Compose\Concerns\InteractsWithRemoteServer;
 use function Laravel\Prompts\multiselect;
@@ -14,6 +15,7 @@ class ComposePublishCommand extends Command
 {
     use CreatesComposeServices;
     use InteractsWithRemoteServer;
+    use InteractsWithEnvFile;
 
     protected $signature = 'compose:publish 
                             {--publish-path=}
@@ -24,12 +26,14 @@ class ComposePublishCommand extends Command
     public function handle()
     {
         $publish_path = $this->option('publish-path') ?: '.';
-        $compose_build_dir = __DIR__.'/../../build';
-        $files = $this->option('files') ?: multiselect(
-            'Select files to publish',
-            ['docker-compose.yml', 'build'],
-            ['docker-compose.yml', 'build'],
-        );
+        $compose_build_dir = __DIR__ . '/../../build';
+        $files = $this->hasOption('files')
+            ? $this->option('files')
+            : multiselect(
+                'Select files to publish',
+                ['docker-compose.yml', 'build'],
+                ['docker-compose.yml', 'build'],
+            );
 
         if (in_array('build', $files)) {
             $this->createDockerfile();
@@ -38,16 +42,20 @@ class ComposePublishCommand extends Command
                 mkdir("{$publish_path}/build", recursive: true);
             }
 
-            foreach(scandir($compose_build_dir) as $file) {
+            foreach (scandir($compose_build_dir) as $file) {
                 if (in_array($file, ['.', '..', 'app.tar'])) {
                     continue;
                 }
-                copy("{$compose_build_dir}/{$file}", "{$publish_path}/build/".basename($file));
+                copy("{$compose_build_dir}/{$file}", "{$publish_path}/build/" . basename($file));
             }
         }
 
         if (in_array('docker-compose.yml', $files)) {
             file_put_contents("{$publish_path}/docker-compose.yml", $this->getComposeYaml());
+        }
+
+        if(!$this->localEnv('COMPOSE_PROFILES')){
+            $this->setEnv('COMPOSE_PROFILES', 'local');
         }
 
         $this->info("Compose files published.");
